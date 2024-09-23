bf0bbc01a7d83548453dd54b2d05c673cdfc531b
tetio <tetio@live.com>
2024-09-23 18:22:50 +0200

    WIP cleaning code

--- application/src/main/java/net/portic/message/processor/application/MultiModuleApplication.java
+++ application/src/main/java/net/portic/message/processor/application/MultiModuleApplication.java
@@ -3,7 +3,6 @@ package net.portic.message.processor.application;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,17 +10,13 @@ import net.portic.message.processor.application.stages.prepare.MSG01Preparer;
 import net.portic.message.processor.application.stages.prepare.MSG12Preparer;
 import net.portic.message.processor.application.stages.prepare.MSG33Preparer;
 import net.portic.message.processor.core.stages.prepare.PreparersFactory;
+
 @Slf4j
-// @SpringBootApplication
 @RequiredArgsConstructor
 @SpringBootApplication
 public class MultiModuleApplication {
 	@Value("${preparer_for_msg12}")
 	private String preparer4Msg12;
-
-	// public static void main(String[] args) {
-	// SpringApplication.run(MultiModuleApplication.class, args);
-	// }
 
 	private final PreparersFactory preparerStageFactory;
 
